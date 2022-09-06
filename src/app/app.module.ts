a452a6193b1d1aa23108a8b79638e307a89c93e9
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-09-06 20:25:07 +0800

    Set up map markers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MarkerService } from './services/marker/marker.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MapComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [MarkerService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { MarkerService } from '../services/marker/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
@@ -9,13 +25,14 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   private map: any;
 
-  constructor() {}
+  constructor(private markerService: MarkerService) {}
 
   ngOnInit(): void {}
 
   // Called after DOM is rendered
   ngAfterViewInit(): void {
     this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
   }
 
   private initMap(): void {
