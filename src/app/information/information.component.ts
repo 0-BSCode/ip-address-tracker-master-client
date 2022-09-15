fcf75d3c3767ca5065f3680f2d48499c1046a095
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-09-15 17:53:20 +0800

    Hooked up frontend to API

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from 'src/types/location';
+import { LocationService } from '../services/location/location.service';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.css']
+  styleUrls: ['./information.component.css'],
 })
 export class InformationComponent implements OnInit {
+  locationInfo?: Location;
 
-  constructor() { }
+  constructor(private locationService: LocationService) {}
 
   ngOnInit(): void {
+    this.fetchLocation('104.28.252.29');
   }
 
+  fetchLocation(ip: string) {
+    this.locationService
+      .fetchLocationDetails(ip)
+      .subscribe((response) => (this.locationInfo = response));
+  }
 }
--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap, catchError } from 'rxjs';
+import { Location } from 'src/types/location';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationService {
+  private url: string = 'http://localhost:4000/getLocationDetails';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  fetchLocationDetails(ip: string): Observable<Location> {
+    return this.httpClient.get<Location>(`${this.url}/${ip}`);
+  }
+
+  private log(message: string) {
+    console.log(message);
+  }
 }
